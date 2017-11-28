45ee4d7f679ee107cb119db52512c9d3c37b74b6
Abe <Abe@172.16.72.32>
2017-11-27 19:30:35 -0700

    d

--- Controller.java
+++ Controller.java
@@ -4,7 +4,10 @@ public class Controller{
   public static void main(String[]args){
   FileManipulation fileman= new FileManipulation();
   TSP tsp=new TSP();
-  int[][]matrix=fileman.readFile("tsp_15");
+  Scanner sc=new Scanner(System.in);
+  System.out.print("What is the name of file where the city matrix is stored?\n(Make sure file is working directory)");
+  String filename=sc.nextLine();
+  int[][]matrix=fileman.readFile(filename);
 
   ArrayList<Integer>citiestouched=new ArrayList<Integer>();
   
@@ -13,8 +16,9 @@ public class Controller{
   
   int smartmin=tsp.findMinPath(matrix,0,matrix.length,0,citiestouched);
   
-  System.out.println(smartmin);
   
+  System.out.println("Mininimum Path Length is: "+smartmin);
+  System.out.println("Order of Cities:");
   for(int i=0; i<tsp.bestroute.size(); i++){
     System.out.println(tsp.bestroute.get(i));
   } 
